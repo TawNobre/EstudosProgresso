d5fde3f9cde8660dd41372d8f2c8e86591d2cbd6
Tawana Nobre <tawnobre1@gmail.com>
2025-02-24 14:45:04 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -3,6 +3,11 @@ altura = 1.70
 peso = 95
 imc = peso / (altura * altura)
 
-print(nome, "tem", altura, "de altura",)
-print("pesa", peso, "quilos e seu imc é",)
+linha_1 = f"{nome} tem {altura:.2f} de altura"
+linha_2 = f"pesa {peso} quilos e seu imc é"
+
+
+print(linha_1)
+print(linha_2)
 print(imc)
+
