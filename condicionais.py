0bd58789f9de970aab97fa25b35a24ee8d96ceca
Tawana Nobre <tawnobre1@gmail.com>
2025-02-24 15:59:27 -0300

    Create condicionais.py

---
+++ condicionais.py
@@ -0,0 +1,13 @@
+#if / elif / else
+# se / se não se / se não
+
+entrada = input("Você quer 'entar' ou 'sair'? ")
+
+if entrada == "entrar":
+    print("Você entrou no sistema")
+
+elif entrada == "sair":
+    print("Você saiu do sistema")
+
+else:
+    print("O usuário não escolheu uma opção válida.")
