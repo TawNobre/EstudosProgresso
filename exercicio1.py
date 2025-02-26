0ca19c75718e178b34998f8bd6b1324e7e31541d
Tawana Nobre <tawnobre1@gmail.com>
2025-02-26 10:54:39 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,12 @@
+primeiro_valor = input("Digite um número: ")
+segundo_valor = input("Digite outro número: ")
+
+if primeiro_valor > segundo_valor:
+    print(f"O {primeiro_valor=} é maior que o {segundo_valor=}")
+
+elif primeiro_valor == segundo_valor:
+    print(f"O {primeiro_valor=} é igual ao {segundo_valor=}")
+
+else:
+    print(f"O {segundo_valor=} é maior que o {primeiro_valor=}")
+    
